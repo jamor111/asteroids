34edbf0959cfaa5e949064a00266f1045082143d
jamor111 <thedzemek@gmail.com>
2025-10-29 18:35:18 +0100

    Next stage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
@@ -7,6 +9,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    nclock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 # GAME LOOP
     while True:
         for event in pygame.event.get():
@@ -14,7 +19,13 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+
+
+
+        nclock.tick(60)
     
 
 
